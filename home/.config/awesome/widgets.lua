5e2a4eb8f287add8da216f68cc2ef5e4c82e8add
Péter Szabados <peter.szabados@ericsson.com>
2017-04-20 13:14:39 +0200

    Close windows on tasklist middle click

--- home/.config/awesome/widgets.lua
+++ home/.config/awesome/widgets.lua
@@ -42,7 +42,6 @@ local main_menu = awful.menu({ items = { { "awesome", awesome_menu, beautiful.aw
 menubar.utils.terminal = variables.terminal -- Set the terminal for applications that require it
 -- }}}
 
-
 -- {{{ Wibar
 -- Create a textclock widget
 local text_clock = wibox.widget.textclock()
@@ -68,22 +67,24 @@ local taglist_buttons = awful.util.table.join(
                 )
 
 local tasklist_buttons = awful.util.table.join(
-                     awful.button({ }, 1, function (c)
-                                              if c == client.focus then
-                                                  c.minimized = true
-                                              else
-                                                  -- Without this, the following
-                                                  -- :isvisible() makes no sense
-                                                  c.minimized = false
-                                                  if not c:isvisible() and c.first_tag then
-                                                      c.first_tag:view_only()
-                                                  end
-                                                  -- This will also un-minimize
-                                                  -- the client, if needed
-                                                  client.focus = c
-                                                  c:raise()
-                                              end
-                                          end),
+                     awful.button({ }, 1,
+                             function (c)
+                                 if c == client.focus then
+                                     c.minimized = true
+                                 else
+                                     -- Without this, the following
+                                     -- :isvisible() makes no sense
+                                     c.minimized = false
+                                     if not c:isvisible() and c.first_tag then
+                                         c.first_tag:view_only()
+                                     end
+                                     -- This will also un-minimize
+                                     -- the client, if needed
+                                     client.focus = c
+                                     c:raise()
+                                 end
+                             end),
+                     awful.button({ }, 2, function(c) c:kill() end),
                      awful.button({ }, 3, client_menu_toggle_fn()),
                      awful.button({ }, 4, function ()
                                               awful.client.focus.byidx(1)
