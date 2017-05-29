4e22084185cbfbebb9a16ba538ca89f936a906f3
Péter Szabados <peter.szabados@ericsson.com>
2017-05-29 15:59:08 +0200

    Clean up

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -271,7 +271,6 @@ local globalkeys = awful.util.table.join(
               {description = "open a terminal", group = "launcher"}),
     awful.key({}, "Print",
             function ()
-                naughty.notify({text=variables.screenshot_tool})
                 awful.spawn.with_shell(variables.screenshot_tool)
             end,
             {description = "Take screenshot", group = "launcher"}),
@@ -280,18 +279,6 @@ local globalkeys = awful.util.table.join(
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
               {description = "quit awesome", group = "awesome"}),
 
-    awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05)          end,
-              {description = "increase master width factor", group = "layout"}),
-    awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05)          end,
-              {description = "decrease master width factor", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "h",     function () awful.tag.incnmaster( 1, nil, true) end,
-              {description = "increase the number of master clients", group = "layout"}),
-    awful.key({ modkey, "Shift"   }, "l",     function () awful.tag.incnmaster(-1, nil, true) end,
-              {description = "decrease the number of master clients", group = "layout"}),
-    awful.key({ modkey, "Control" }, "h",     function () awful.tag.incncol( 1, nil, true)    end,
-              {description = "increase the number of columns", group = "layout"}),
-    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
-              {description = "decrease the number of columns", group = "layout"}),
     awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
               {description = "select next", group = "layout"}),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
