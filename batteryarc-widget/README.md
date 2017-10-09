2b4dba4853fc1840c5aef300b747c5a182655ba4
pmakhov <pavel.makhov@savoirfairelinux.com>
2017-10-09 17:14:03 -0400

    batteryarc widget

--- batteryarc-widget/README.md
+++ batteryarc-widget/README.md
@@ -1,2 +1,44 @@
 # Batteryarc widget
-:hammer: Work in progress...:construction:
+
+This widget is more informative version of [battery widget](https://github.com/streetturtle/awesome-wm-widgets/tree/master/battery-widget).
+
+Depending of the battery status it could look following ways:
+
+ - ![10_d](./10_d.png) - less than 15 percent
+ - ![10_c](./10_c.png) - less than 15 percent, charging
+ - ![20_d](./20_d.png) - between 15 and 40 percent
+ - ![20_c](./20_c.png) - between 15 and 40 percent, charging
+ - ![80_d](./80_d.png) - more than 40 percent
+ - ![80_c](./80_c.png) - more than 40 percent, charging
+
+Widget uses following beautiful variables with values:
+
+```lua
+theme.widget_main_color = "#74aeab"
+theme.widget_red = "#e53935"
+theme.widget_yelow = "#c0ca33"
+theme.widget_green = "#43a047"
+theme.widget_black = "#000000"
+theme.widget_transparent = "#00000000"
+```
+
+which means that you need to copy the code above and paste it in your **theme.lua**. Otherwise you can change colors directly in the widget.
+
+## Installation
+
+Clone repo, include widget and use it in **rc.lua**:
+
+```lua
+require("volumearc")
+...
+s.mytasklist, -- Middle widget
+	{ -- Right widgets
+    	layout = wibox.layout.fixed.horizontal,
+		...
+		batteryarc_widget,
+		...
+```
+
+## Troubleshooting
+
+In case of any doubts or questions don't hesitate to raise an [issue](https://github.com/streetturtle/awesome-wm-widgets/issues/new).
--- batteryarc-widget/batteryarc.lua
+++ batteryarc-widget/batteryarc.lua
@@ -1,23 +1,32 @@
 local awful = require("awful")
+local beautiful = require("beautiful")
 local naughty = require("naughty")
 local wibox = require("wibox")
 local watch = require("awful.widget.watch")
 
-local USERNAME = os.getenv("USER")
+local HOME = os.getenv("HOME")
 
-batteryarc_widget = wibox.widget {
-    {
-        id = "txt",
-        text = "1",
-        font = "Play 5",
-        widget = wibox.widget.textbox
-    },
+-- only text
+local text = wibox.widget {
+    id = "txt",
+    font = "Play 5",
+    widget = wibox.widget.textbox
+}
+
+-- mirror the text, because the whole widget will be mirrored after
+local mirrored_text = wibox.container.mirror(text, { horizontal = true })
+
+-- mirrored text with background
+local mirrored_text_with_background = wibox.container.background(mirrored_text)
+
+local batteryarc = wibox.widget {
+    mirrored_text_with_background,
     max_value = 1,
     rounded_edge = true,
     thickness = 2,
     start_angle = 4.71238898, -- 2pi*3/4
-    forced_height = 16,
-    forced_width = 16,
+    forced_height = 17,
+    forced_width = 17,
     bg = "#ffffff11",
     paddings = 2,
     widget = wibox.container.arcchart,
@@ -26,17 +35,36 @@ batteryarc_widget = wibox.widget {
     end,
 }
 
-watch(
-    "acpi", 10,
+-- mirror the widget, so that chart value increases clockwise
+batteryarc_widget = wibox.container.mirror(batteryarc, { horizontal = true })
+
+watch("acpi", 10,
     function(widget, stdout, stderr, exitreason, exitcode)
         local batteryType
         local _, status, charge_str, time = string.match(stdout, '(.+): (%a+), (%d?%d%d)%%,? ?.*')
         local charge = tonumber(charge_str)
         widget.value = charge / 100
-        widget.txt.text = charge
+        if status == 'Charging' then
+            mirrored_text_with_background.bg = beautiful.widget_green
+            mirrored_text_with_background.fg = beautiful.widget_black
+        else
+            mirrored_text_with_background.bg = beautiful.widget_transparent
+            mirrored_text_with_background.fg = beautiful.widget_main_color
+        end
+
+        if charge < 15 then
+            batteryarc.colors = { beautiful.widget_red }
+            if status ~= 'Charging' then
+                show_battery_warning()
+            end
+        elseif charge > 15 and charge < 40 then
+            batteryarc.colors = { beautiful.widget_yellow }
+        else
+            batteryarc.colors = { beautiful.widget_main_color }
+        end
+        text.text = charge
     end,
-    batteryarc_widget
-)
+    batteryarc)
 
 -- Popup with battery info
 -- One way of creating a pop-up notification - naughty.notify
@@ -44,17 +72,18 @@ local notification
 function show_battery_status()
     awful.spawn.easy_async([[bash -c 'acpi']],
         function(stdout, _, _, _)
-            notification = naughty.notify{
-                text =  stdout,
+            notification = naughty.notify {
+                text = stdout,
                 title = "Battery status",
-                timeout = 5, hover_timeout = 0.5,
+                timeout = 5,
+                hover_timeout = 0.5,
                 width = 200,
             }
-        end
-    )
+        end)
 end
-batteryarc_widget:connect_signal("mouse::enter", function() show_battery_status() end)
-batteryarc_widget:connect_signal("mouse::leave", function() naughty.destroy(notification) end)
+
+batteryarc:connect_signal("mouse::enter", function() show_battery_status() end)
+batteryarc:connect_signal("mouse::leave", function() naughty.destroy(notification) end)
 
 -- Alternative to naughty.notify - tooltip. You can compare both and choose the preferred one
 
@@ -67,12 +96,13 @@ batteryarc_widget:connect_signal("mouse::leave", function() naughty.destroy(noti
 
 --[[ Show warning notification ]]
 function show_battery_warning()
-    naughty.notify{
-        icon = "/home/" .. USERNAME .. "/.config/awesome/nichosi.png",
-        icon_size=100,
+    naughty.notify {
+        icon = HOME .. "/.config/awesome/nichosi.png",
+        icon_size = 100,
         text = "Huston, we have a problem",
         title = "Battery is dying",
-        timeout = 5, hover_timeout = 0.5,
+        timeout = 5,
+        hover_timeout = 0.5,
         position = "bottom_right",
         bg = "#F06060",
         fg = "#EEE9EF",
