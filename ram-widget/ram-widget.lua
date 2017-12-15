3f007ecc7625e3f5e7d8469d493bd908daf0b334
pmakhov <pavel.makhov@savoirfairelinux.com>
2017-12-14 21:05:43 -0500

    some improvements

--- ram-widget/ram-widget.lua
+++ ram-widget/ram-widget.lua
@@ -34,33 +34,44 @@ local w = wibox {
 }
 
 w:setup {
-    border_width = 0,
-    colors = {
-        '#74aeab',
-        '#6eaaa7',
-        '#5ea19d',
-        '#55918e',
-        '#4b817e',
+    {
+        border_width = 0,
+        colors = {
+            '#74aeab',
+            '#6eaaa7',
+            '#5ea19d',
+            '#55918e',
+            '#4b817e',
+        },
+        display_labels = false,
+        forced_width = 25,
+        id = 'pie',
+        widget = wibox.widget.piechart
     },
-    display_labels = false,
-    forced_width = 25,
-    id = 'pie',
-    widget = wibox.widget.piechart
+    {
+        text = 'Hello',
+        widget = wibox.widget.textbox
+    },
+    id = 'popup',
+    layout = wibox.layout.stack
 }
+
+local function getPercentage(value)
+    return math.floor(value / total * 100 + 0.5) .. '%'
+end
 
 ramgraph_widget:buttons(
     awful.util.table.join(
         awful.button({}, 1, function()
             awful.placement.top_right(w, { margins = {top = 25, right = 10}})
-            w.pie.data_list = {
-                {'used' , used},
-                {'free' , free},
-                {'shared' , shared},
-                {'buff_cache' , buff_cache},
-                {'available' , available
-                }
+            w.popup.pie.data_list = {
+                {'used ' .. getPercentage(used), used},
+                {'free ' .. getPercentage(free), free},
+--                {'shared ' .. getPercentage(shared), shared},
+                {'buff_cache ' .. getPercentage(buff_cache), buff_cache},
+--                {'available ' .. getPercentage(available), available}
             }
-            w.pie.display_labels = true
+            w.popup.pie.display_labels = true
             w.visible = true
         end),
         awful.button({}, 3, function()
