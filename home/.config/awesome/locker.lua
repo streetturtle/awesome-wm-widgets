581ad5bf9d33d60f7b7e84a670bb6dba05925e56
Péter Szabados <peter.szabados@ericsson.com>
2018-01-15 16:46:50 +0100

    Fix locking when disabled

--- home/.config/awesome/locker.lua
+++ home/.config/awesome/locker.lua
@@ -11,33 +11,52 @@ local args = {}
 local enable_command = "xautolock -enable"
 local disable_command = "xautolock -disable"
 local lock_command = "xautolock -locknow"
+local locked = false
+local disabled = false
+
+local function do_lock()
+    gears.timer.start_new(1,
+            function()
+                if locked then
+                    return false
+                end
+                awful.spawn(lock_command)
+                return true
+            end)
+end
 
 function locker.lock(callback)
     locker.callback = callback
-    async.spawn_and_get_output(enable_command,
-        function()
-            local command
-            async.spawn_and_get_output(lock_command,
-                function()
-                    if locker.prevent_idle:is_locked() then
-                        awful.spawn(disable_command)
-                    end
-                end)
-        end)
+    if disabled then
+        awful.spawn(enable_command)
+        do_lock()
+    else
+        awful.spawn(lock_command)
+    end
 end
 
 locker.prevent_idle = Semaphore(
         function()
+            disabled = true
             awful.spawn(disable_command)
         end,
         function()
+            disabled = false
             awful.spawn(enable_command)
         end)
 
-function locker.run_callback()
+function locker._run_callback()
+    locked = true
     if locker.callback then
         locker.callback()
         locker.callback = nil
+    end
+end
+
+function locker._on_lock_finished()
+    locked = false
+    if locker.prevent_idle:is_locked() then
+        awful.spawn(disable_command)
     end
 end
 
