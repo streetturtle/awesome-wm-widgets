dcd6f6269018c6d5f3257c77a69624cf76fcc5d2
streetturtle <streetturtle@gmail.com>
2018-01-20 21:52:20 -0500

    remove network widget

--- network-widget/networkio.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ACTIVE_INTERFACE="$(ip -o link show | awk '$9~/UP/{sub(":","",$2);print $2}')"
-
-IO="$(cat /proc/net/dev | grep ${ACTIVE_INTERFACE} | sed "s/.*://" | awk '{printf $1";"$9}')"
-
-echo "${IO}"
