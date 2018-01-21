4239563581439be21de16642b12e92dbf7615310
streetturtle <streetturtle@gmail.com>
2018-01-20 21:42:28 -0500

    update README

---
+++ secrets.lua
@@ -0,0 +1,6 @@
+local secrets = {
+    translate_widget_api_key = 'trnsl.1.1.20170708T010252Z.8b3fcacab3396ad1.88df3f48339672bce016a0e85a4db3aeb34dc7ae',
+    weather_widget_api_key = 'c3d7320b359da4e48c2d682a04076576'
+}
+
+return secrets
