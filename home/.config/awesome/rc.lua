0faf1d88f739557351c7478ff698f363404aca9e
Péter Stabados <kanirigungi@gmail.com>
2018-03-31 13:46:02 +0200

    Add brighrness controls

--- home/.config/awesome/rc.lua
+++ home/.config/awesome/rc.lua
@@ -329,6 +329,14 @@ local globalkeys = awful.util.table.join(
     awful.key({ }, "XF86AudioMute",         APW.ToggleMute,
             {description="Toggle Mute", group="volume"}),
 
+    --- Brightness
+    awful.key({ }, "XF86MonBrightnessUp",
+            function() awful.spawn("xbacklight -inc 10") end,
+            {description="Increase brightness", group="screen"}),
+    awful.key({ }, "XF86MonBrightnessDown",
+            function() awful.spawn("xbacklight -dec 10") end,
+            {description="Decrease brightness", group="screen"}),
+
     -- Prompt
     awful.key({ modkey },            "r",     function () awful.screen.focused().mypromptbox:run() end,
               {description = "run prompt", group = "launcher"}),
