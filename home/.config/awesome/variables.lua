4e22306f0075a1b2fcc823241bcc8f0fed69e199
Péter Szabados <kangirigungi@gmail.com>
2018-04-11 17:51:22 +0200

    Add terminator

--- home/.config/awesome/variables.lua
+++ home/.config/awesome/variables.lua
@@ -7,8 +7,9 @@ variables.config_dir = os.getenv("HOME") .. "/.config/awesome"
 -- This is used later as the default terminal and editor to run.
 variables.terminal = command.get_available_command({
         {command="gnome-terminal"},
-        {command="konsole"},
         {command="x-terminal-emulator"},
+        {command="terminator"},
+        {command="konsole"},
         {command="xterm"},
     })
 variables.browser = command.get_available_command({
