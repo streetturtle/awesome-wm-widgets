429e6eb54a5bab17d7adba8956fcbc0a63257ed5
streetturtle <streetturtle@gmail.com>
2019-05-02 21:35:30 -0400

    external config for brightness and brightnessarc

--- brightnessarc-widget/brightnessarc.lua
+++ brightnessarc-widget/brightnessarc.lua
@@ -2,7 +2,7 @@
 -- Brightness Widget for Awesome Window Manager
 -- Shows the brightness level of the laptop display
 -- More details could be found here:
--- https://github.com/streetturtle/awesome-wm-widgets/tree/master/brightnessarc-widget
+-- https://github.com/streetturtle/awesome-wm-widgets/tree/master/widget-widget
 
 -- @author Pavel Makhov
 -- @copyright 2019 Pavel Makhov
@@ -17,38 +17,56 @@ local GET_BRIGHTNESS_CMD = "light -G" -- "xbacklight -get"
 local INC_BRIGHTNESS_CMD = "light -A 5" -- "xbacklight -inc 5"
 local DEC_BRIGHTNESS_CMD = "light -U 5" -- "xbacklight -dec 5"
 
-local icon = {
-    id = "icon",
-    image = PATH_TO_ICON,
-    resize = true,
-    widget = wibox.widget.imagebox,
-}
+local widget = {}
 
-local brightnessarc = wibox.widget {
-    icon,
-    max_value = 1,
-    thickness = 2,
-    start_angle = 4.71238898, -- 2pi*3/4
-    forced_height = 18,
-    forced_width = 18,
-    bg = "#ffffff11",
-    paddings = 2,
-    widget = wibox.container.arcchart
-}
+local function worker(args)
 
-local update_widget = function(widget, stdout)
-    local brightness_level = string.match(stdout, "(%d?%d?%d?)")
-    brightness_level = tonumber(string.format("% 3d", brightness_level))
+    local args = args or {}
 
-    widget.value = brightness_level / 100;
-end,
+    local get_brightness_cmd = args.get_brightness_cmd or GET_BRIGHTNESS_CMD
+    local inc_brightness_cmd = args.inc_brightness_cmd or INC_BRIGHTNESS_CMD
+    local dec_brightness_cmd = args.dec_brightness_cmd or DEC_BRIGHTNESS_CMD
+    local path_to_icon = args.path_to_icon or PATH_TO_ICON
 
-brightnessarc:connect_signal("button::press", function(_, _, _, button)
-    if (button == 4) then spawn(INC_BRIGHTNESS_CMD, false)
-    elseif (button == 5) then spawn(DEC_BRIGHTNESS_CMD, false)
-    end
-end)
+    local icon = {
+        id = "icon",
+        image = path_to_icon,
+        resize = true,
+        widget = wibox.widget.imagebox,
+    }
 
-watch(GET_BRIGHTNESS_CMD, 1, update_widget, brightnessarc)
+    widget = wibox.widget {
+        icon,
+        max_value = 1,
+        thickness = 2,
+        start_angle = 4.71238898, -- 2pi*3/4
+        forced_height = 18,
+        forced_width = 18,
+        bg = "#ffffff11",
+        paddings = 2,
+        widget = wibox.container.arcchart
+    }
 
-return brightnessarc
+    local update_widget = function(widget, stdout)
+        local brightness_level = string.match(stdout, "(%d?%d?%d?)")
+        brightness_level = tonumber(string.format("% 3d", brightness_level))
+
+        widget.value = brightness_level / 100;
+    end,
+
+    widget:connect_signal("button::press", function(_, _, _, button)
+        if (button == 4) then
+            spawn(inc_brightness_cmd, false)
+        elseif (button == 5) then
+            spawn(dec_brightness_cmd, false)
+        end
+    end)
+
+    watch(get_brightness_cmd, 1, update_widget, widget)
+
+    return widget
+end
+
+return setmetatable(widget, { __call = function(_, ...)
+    return worker(...)
+end })
