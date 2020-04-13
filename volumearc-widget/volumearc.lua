edb5283c6d644a182d9d2c789b14bdeb372c4928
streetturtle <streetturtle@gmail.com>
2020-04-13 08:45:54 -0400

    volumearc - inc/dec volume quietly

--- volumearc-widget/volumearc.lua
+++ volumearc-widget/volumearc.lua
@@ -15,9 +15,9 @@ local watch = require("awful.widget.watch")
 local wibox = require("wibox")
 
 local GET_VOLUME_CMD = 'amixer -D pulse sget Master'
-local INC_VOLUME_CMD = 'amixer -D pulse sset Master 5%+'
-local DEC_VOLUME_CMD = 'amixer -D pulse sset Master 5%-'
-local TOG_VOLUME_CMD = 'amixer -D pulse sset Master toggle'
+local INC_VOLUME_CMD = 'amixer -q -D pulse sset Master 5%+'
+local DEC_VOLUME_CMD = 'amixer -q -D pulse sset Master 5%-'
+local TOG_VOLUME_CMD = 'amixer -q -D pulse sset Master toggle'
 
 local PATH_TO_ICON = "/usr/share/icons/Arc/status/symbolic/audio-volume-muted-symbolic.svg"
 
