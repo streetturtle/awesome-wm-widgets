88d6697d8db81343cb44b637ebcb376176807fe5
streetturtle <streetturtle@gmail.com>
2020-11-14 13:29:31 -0500

    [experiments-volume] fix muting

--- experiments/volume/widgets/icon-widget.lua
+++ experiments/volume/widgets/icon-widget.lua
@@ -15,21 +15,29 @@ function widget.get_widget()
         valign = 'center',
         layout = wibox.container.place,
         set_volume_level = function(self, new_value)
-            local new_value_num = tonumber(new_value)
             local volume_icon_name = ''
-            if (new_value_num >= 0 and new_value_num < 33) then
-                volume_icon_name="audio-volume-low-symbolic"
-            elseif (new_value_num < 66) then
-                volume_icon_name="audio-volume-medium-symbolic"
+            if self.is_muted then
+                volume_icon_name = 'audio-volume-muted-symbolic.svg'
             else
-                volume_icon_name="audio-volume-high-symbolic"
+                local new_value_num = tonumber(new_value)
+                if (new_value_num >= 0 and new_value_num < 33) then
+                    volume_icon_name="audio-volume-low-symbolic"
+                elseif (new_value_num < 66) then
+                    volume_icon_name="audio-volume-medium-symbolic"
+                else
+                    volume_icon_name="audio-volume-high-symbolic"
+                end
             end
             self:get_children_by_id('icon')[1]:set_image(WIDGET_DIR .. volume_icon_name .. '.svg')
         end,
-        mute = function(self) self:get_children_by_id('icon')[1]:set_image(WIDGET_DIR .. 'audio-volume-muted-symbolic.svg') end,
-        unmute = function() end,
+        mute = function(self)
+            self.is_muted = true
+            self:get_children_by_id('icon')[1]:set_image(WIDGET_DIR .. 'audio-volume-muted-symbolic.svg')
+        end,
+        unmute = function(self)
+            self.is_muted = false
+        end
     }
-
 end
 
 return widget
