692767d09ebdc501a01a971b899a86dbcaef1237
streetturtle <streetturtle@gmail.com>
2020-12-18 21:13:21 -0500

    new widget - logout-menu

---
+++ logout-menu-widget/README.md
@@ -0,0 +1,45 @@
+# Logout Menu Widget
+
+This widget shows a menu with options to log out from the current session, lock, reboot, suspend and power off the computer, similar to [logout-popup-widget](https://github.com/streetturtle/awesome-wm-widgets/tree/master/logout-popup-widget):
+
+![demo](./logout-menu.gif)
+
+## Installation
+
+Clone this repo (if not cloned yet) **./.config/awesome/**
+
+```bash
+cd ./.config/awesome/
+git clone https://github.com/streetturtle/awesome-wm-widgets
+```
+Then add the widget to the wibar:
+
+```lua
+local logout_menu_widget = require("awesome-wm-widgets.logout-menu-widget.logout-menu")
+
+s.mytasklist, -- Middle widget
+    { -- Right widgets
+        layout = wibox.layout.fixed.horizontal,
+        ...
+        -- default
+        logout_menu_widget(),
+        -- custom
+        logout_menu_widget{
+            font = 'Play 14',
+            onlock = function() awful.spawn.with_shell('i3lock-fancy') end
+        }
+        ...
+```
+
+## Customization
+
+It is possible to customize the widget by providing a table with all or some of the following config parameters:
+
+| Name | Default | Description |
+|---|---|---|
+| `font` | `beautiful.font` | Font of the menu items |
+| `onlogout` | `function() awesome.quit() end` | Function which is called when the logout item is clicked |
+| `onlock` | `function() awful.spawn.with_shell("i3lock") end` | Function which is called when the lock item is clicked |
+| `onreboot` | `function() awful.spawn.with_shell("reboot") end` | Function which is called when the reboot item is clicked |
+| `onsuspend` | `function() awful.spawn.with_shell("systemctl suspend") end` | Function which is called when the suspend item is clicked |
+| `onpoweroff` | `function() awful.spawn.with_shell("shutdown now") end` | Function which is called when the poweroff item is clicked |
---
+++ logout-menu-widget/logout-menu.lua
@@ -0,0 +1,129 @@
+-------------------------------------------------
+-- Logout Menu Widget for Awesome Window Manager
+-- More details could be found here:
+-- https://github.com/streetturtle/awesome-wm-widgets/tree/master/logout-menu-widget
+
+-- @author Pavel Makhov
+-- @copyright 2020 Pavel Makhov
+-------------------------------------------------
+
+local awful = require("awful")
+local wibox = require("wibox")
+local spawn = require("awful.spawn")
+local gears = require("gears")
+local beautiful = require("beautiful")
+
+local HOME = os.getenv('HOME')
+local ICON_DIR = HOME .. '/.config/awesome/awesome-wm-widgets/logout-menu-widget/icons/'
+
+local logout_menu_widget = wibox.widget {
+    {
+        image = ICON_DIR .. 'power_w.svg',
+        resize = true,
+        widget = wibox.widget.imagebox,
+    },
+    margins = 4,
+    layout = wibox.container.margin
+}
+
+local popup = awful.popup {
+    ontop = true,
+    visible = false,
+    shape = function(cr, width, height)
+        gears.shape.rounded_rect(cr, width, height, 4)
+    end,
+    border_width = 1,
+    border_color = beautiful.bg_focus,
+    maximum_width = 400,
+    offset = { y = 5 },
+    widget = {}
+}
+local rows = { layout = wibox.layout.fixed.vertical }
+
+local function worker(user_args)
+
+    local args = user_args or {}
+
+    local font = args.font or beautiful.font
+
+    local onlogout = args.onlogout or function () awesome.quit() end
+    local onlock = args.onlock or function() awful.spawn.with_shell("i3lock") end
+    local onreboot = args.onreboot or function() awful.spawn.with_shell("reboot") end
+    local onsuspend = args.onsuspend or function() awful.spawn.with_shell("systemctl suspend") end
+    local onpoweroff = args.onpoweroff or function() awful.spawn.with_shell("shutdown now") end
+
+    local menu_items = {
+        { name = 'Log out', icon_name = 'log-out.svg', command = onlogout },
+        { name = 'Lock', icon_name = 'lock.svg', command = onlock },
+        { name = 'Reboot', icon_name = 'refresh-cw.svg', command = onreboot },
+        { name = 'Suspend', icon_name = 'moon.svg', command = onsuspend },
+        { name = 'Power off', icon_name = 'power.svg', command = onpoweroff },
+    }
+
+    for _, item in ipairs(menu_items) do
+
+        local row = wibox.widget {
+            {
+                {
+                    {
+                        image = ICON_DIR .. item.icon_name,
+                        resize = false,
+                        widget = wibox.widget.imagebox
+                    },
+                    {
+                        text = item.name,
+                        font = font,
+                        widget = wibox.widget.textbox
+                    },
+                    spacing = 12,
+                    layout = wibox.layout.fixed.horizontal
+                },
+                margins = 8,
+                layout = wibox.container.margin
+            },
+            bg = beautiful.bg_normal,
+            widget = wibox.container.background
+        }
+
+        row:connect_signal("mouse::enter", function(c) c:set_bg(beautiful.bg_focus) end)
+        row:connect_signal("mouse::leave", function(c) c:set_bg(beautiful.bg_normal) end)
+
+        local old_cursor, old_wibox
+        row:connect_signal("mouse::enter", function()
+            local wb = mouse.current_wibox
+            old_cursor, old_wibox = wb.cursor, wb
+            wb.cursor = "hand1"
+        end)
+        row:connect_signal("mouse::leave", function()
+            if old_wibox then
+                old_wibox.cursor = old_cursor
+                old_wibox = nil
+            end
+        end)
+
+        row:buttons(awful.util.table.join(awful.button({}, 1, function()
+            popup.visible = not popup.visible
+            item.command()
+        end)))
+
+        table.insert(rows, row)
+    end
+    popup:setup(rows)
+
+    logout_menu_widget:buttons(
+            awful.util.table.join(
+                    awful.button({}, 1, function()
+                        if popup.visible then
+                            popup.visible = not popup.visible
+                        else
+                            popup:move_next_to(mouse.current_widget_geometry)
+                        end
+                    end)
+            )
+    )
+
+    return logout_menu_widget
+
+end
+
+return setmetatable(logout_menu_widget, { __call = function(_, ...) return worker(...) end })
