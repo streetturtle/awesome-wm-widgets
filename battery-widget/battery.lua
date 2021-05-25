1a4f8e04fdca007f72f8b72dcd7624d53836e1e3
eylles <ed.ylles1997@gmail.com>
2021-05-24 21:58:37 -0600

    use time to full charge to decide if charging icon, cleaned up upower call

--- battery-widget/battery.lua
+++ battery-widget/battery.lua
@@ -191,29 +191,15 @@ local function worker(user_args)
             timeout,
             function(widget, stdout)
                 local bat_now = {
-                    present      = "N/A",
                     state        = "N/A",
-                    warninglevel = "N/A",
-                    energy       = "N/A",
-                    energyfull   = "N/A",
-                    energyrate   = "N/A",
-                    voltage      = "N/A",
+                    timefull     = "N/A",
                     percentage   = "N/A",
-                    capacity     = "N/A",
-                    icon         = "N/A"
                 }
 
                 for k, v in string.gmatch(stdout, '([%a]+[%a|-]+):%s*([%a|%d]+[,|%a|%d]-)') do
-                    if     k == "present"       then bat_now.present      = v
-                    elseif k == "state"         then bat_now.state        = v
-                    elseif k == "warning-level" then bat_now.warninglevel = v
-                    elseif k == "energy"        then bat_now.energy       = string.gsub(v, ",", ".") -- Wh
-                    elseif k == "energy-full"   then bat_now.energyfull   = string.gsub(v, ",", ".") -- Wh
-                    elseif k == "energy-rate"   then bat_now.energyrate   = string.gsub(v, ",", ".") -- W
-                    elseif k == "voltage"       then bat_now.voltage      = string.gsub(v, ",", ".") -- V
+                    if     k == "state"         then bat_now.state        = v
+                    elseif k == 'full'          then bat_now.timefull     = v
                     elseif k == "percentage"    then bat_now.percentage   = tonumber(v)              -- %
-                    elseif k == "capacity"      then bat_now.capacity     = string.gsub(v, ",", ".") -- %
-                    elseif k == "icon-name"     then bat_now.icon         = v
                     end
                 end
 
@@ -238,7 +224,7 @@ local function worker(user_args)
                 elseif (charge >= 80 and charge <= 100) then batteryType = "battery-full%s-symbolic"
                 end
 
-                if status == 'charging' then
+                if (status == 'charging' and bat_now.timefull ~= "N/A") then
                     batteryType = string.format(batteryType, '-charging')
                 else
                     batteryType = string.format(batteryType, '')
