e66effbf9ac1691af00b04946581a5314001b7be
streetturtle <streetturtle@users.noreply.github.com>
2022-01-23 20:11:57 -0500

    Update update_site.sh

--- scripts/update_site.sh
+++ scripts/update_site.sh
@@ -4,9 +4,10 @@ for D in *; do
         echo "${D}"
         cp ${D}/README.md ./_widgets/${D}.md
         sed -i '1s/^/---\nlayout: page\n---\n/' ./_widgets/${D}.md
-        mkdir -p ./assets/img/screenshots/${D}
 
-        find ${D}/ \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -exec cp '{}' ./assets/img/screenshots/${D} \;
+        mkdir -p ./assets/img/widgets/screenshots/${D}
+
+        find ${D}/ \( -name '*.jpg' -o -name '*.png' -o -name '*.gif' \) -exec cp '{}' ./assets/img/widgets/screenshots/${D} \;
 
         sed -i "s/](\.\(\/screenshots\)\{0,1\}/](..\/awesome-wm-widgets\/assets\/img\/screenshots\/$D/g" ./_widgets/${D}.md
     fi
