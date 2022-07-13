0ad971c5b7497b37a484e1e42540ce126c1eb00a
Christopher Crockett <git@chray.cc>
2022-07-13 15:34:34 -0400

    Added title_scroll_length option to spotify-widget
    Allows specifying a max title length before the widget begins to scroll it
    If -1 is specified, the widget will never scroll (similar to max_length)
    
    Note that ONLY the song title will ever scroll and so the maximum length
    is only in respect to the song length, not the overall content length.
    (this is the pre-existing behavior which has not been touched in this PR)

--- spotify-widget/spotify.lua
+++ spotify-widget/spotify.lua
@@ -37,12 +37,35 @@ local function worker(user_args)
     local dim_when_paused = args.dim_when_paused == nil and false or args.dim_when_paused
     local dim_opacity = args.dim_opacity or 0.2
     local max_length = args.max_length or 15
+    local title_scroll_length = args.title_scroll_length or 100
     local show_tooltip = args.show_tooltip == nil and true or args.show_tooltip
     local timeout = args.timeout or 1
 
     local cur_artist = ''
     local cur_title = ''
     local cur_album = ''
+
+    local title_widget = (function()
+            if title_scroll_length >= 0 then
+                return {
+                    layout = wibox.container.scroll.horizontal,
+                    max_size = title_scroll_length,
+                    step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
+                    speed = 40,
+                    {
+                        id = 'titlew',
+                        font = font,
+                        widget = wibox.widget.textbox
+                    }
+                }
+            else
+                return {
+                    id = 'titlew',
+                    font = font,
+                    widget = wibox.widget.textbox
+                }
+            end
+    end)()
 
     spotify_widget = wibox.widget {
         {
@@ -54,17 +77,7 @@ local function worker(user_args)
             id = "icon",
             widget = wibox.widget.imagebox,
         },
-        {
-            layout = wibox.container.scroll.horizontal,
-            max_size = 100,
-            step_function = wibox.container.scroll.step_functions.waiting_nonlinear_back_and_forth,
-            speed = 40,
-            {
-                id = 'titlew',
-                font = font,
-                widget = wibox.widget.textbox
-            }
-        },
+        title_widget,
         layout = wibox.layout.align.horizontal,
         set_status = function(self, is_playing)
             self.icon.image = (is_playing and play_icon or pause_icon)
