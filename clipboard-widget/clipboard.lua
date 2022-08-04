b871ed7853a0f480a20b207522f72aadfbbdf638
threedots <threedotsellipsis@protonmail.com>
2022-08-04 19:55:28 +0500

    Displaying a popup with item

--- clipboard-widget/clipboard.lua
+++ clipboard-widget/clipboard.lua
@@ -13,11 +13,53 @@ local watch = require("awful.widget.watch")
 local clipboard_widget = {}
 local menu_items = {}
 
+local function build_popup()
+    local popup = awful.popup {
+        ontop = true,
+        visible = false,
+        shape = function(cr, width, height)
+            gears.shape.rounded_rect(cr, width, height, 4)
+        end,
+        border_width = 1,
+        border_color = beautiful.bg_focus,
+        maximum_width = 400,
+        offset = { y = 5 },
+        widget = {}
+    }
+
+    local row = wibox.widget {
+        {
+            {
+                {
+                    text = "Hello",
+                    widget = wibox.widget.textbox
+                },
+                spacing = 16,
+                layout = wibox.layout.fixed.horizontal
+            },
+            margins = 16,
+            widget = wibox.container.margin
+        },
+        bg = beautiful.bg_normal,
+        widget = wibox.container.background
+    }
+
+    local rows = { row, layout = wibox.layout.fixed.vertical }
+
+    -- Add rows to the popup
+    popup:setup(rows)
+
+    return popup
+end
+
 local function worker()
     clipboard_widget = wibox.widget {
         widget = wibox.widget.textbox,
         text = "Clip "
     }
+
+    local popup = build_popup()
+    popup.visible = true
 
     return clipboard_widget
 end
